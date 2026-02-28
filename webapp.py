8cbee996980b1714443cc469a2fda1c156ba14bc
Afonso Duarte <afonso.gomes.duarte@tecnico.ulisboa.pt>
2026-02-28 01:26:51 +0000

    fixes

--- webapp.py
+++ webapp.py
@@ -1061,7 +1061,7 @@ with aba2:
                 if img_selecionada:
                     st.session_state.chat_session_bulk = criar_chat_session(item_dados['Raw'])
                     nome_prod = item_dados['Item']
-                    veredito = item_dados['Veredict']
+                    veredito = item_dados['Verdict']
                     boas_vindas = f"👋 I'm ready to chat about **{nome_prod}** {veredito}. \n\nBased on the references I found, do you want to know more about the pricing strategy or how to prepare the ad?"
                     st.session_state.chat_history_bulk.append({"role": "assistant", "content": boas_vindas})
 
